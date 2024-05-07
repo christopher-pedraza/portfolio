511ba0aa6a37b06aae80569d03cddeed8d454dba
Christopher Pedraza <gabalixreborn@gmail.com>
2024-05-07 00:37:50 -0600

    chore: Update dependencies and add lazy loading for images

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 
 import Title from "./components/Title/Title";
+import DisplayCard from "./components/DisplayCard/DisplayCard";
 
 function App() {
     return (
         <>
             <Title />
+            <div className="py-20 flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8">
+                <DisplayCard />
+            </div>
         </>
     );
 }
--- portfolio/src/components/Title/Title.jsx
+++ portfolio/src/components/Title/Title.jsx
@@ -3,10 +3,10 @@ import { SparklesCore } from "../aceternity/sparkles";
 function Title() {
     return (
         <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-            <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+            <h1 className="md:text-5xl text-1xl lg:text-7xl font-bold text-center text-white relative z-20 mb-4">
                 Christopher Pedraza
             </h1>
-            <div className="w-[40rem] h-40 relative">
+            <div className="w-[40rem] h-30 relative">
                 {/* Gradients */}
                 <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
                 <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
